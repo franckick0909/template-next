0a44b4cb064ddd78ece4590f1f2d37582a1a2fb1
franck Chapelon <franckick2@gmail.com>
2024-04-23 18:55:37 +0200

    commit 6

--- src/components/collapse.jsx
+++ src/components/collapse.jsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Image from "next/image";
+import imgprojet from "../images/kasa3.png";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Collapse = () => {
   const [openCollapse, setOpenCollapse] = useState(false);
@@ -12,87 +14,94 @@ const Collapse = () => {
 
   const list = {
     visible: {
-      height: "0%",
-      y: 0,
+      height: "auto",
       opacity: 1,
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.3,
-      },
     },
     hidden: {
-      height: "100%",
-      y: -100,
-      opacity: 0,
-      transition: {
-        when: "afterChildren",
-      },
+      opacity: 1,
+      height: 0,
     },
   };
 
   const item = {
-    visible: { opacity: 1, y: 0 },
-    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.2,
+        duration: 1,
+        staggerDirection: -1,
+
+      },
+    },
+    hidden: {
+      opacity: 0,
+      y: 200,
+      transition: {
+        duration: 1,
+        staggerDirection: 1,
+        staggerChildren: 0.1,
+    },
+    },
   };
 
   return (
-    <div className="w-full h-auto flex flex-col gap-4 overflow-hidden border-y-[0.5px] border-slate-900 py-8">
-      <div className="flex justify-between items-center ">
-        <h1 className="text-2xl font-bold">Mon Portfolio #1</h1>
-        <h2>React, Sass, Framer-motion</h2>
-        <h2>2024</h2>
-        <button
-          className="text-white bg-slate-400 px-4 py-2 rounded-md"
-          onClick={handleCollapse}>
-          En voir +
-        </button>
-      </div>
-
-      <motion.div
-        className={`w-full h-auto flex-col bg-blue-600 ${
-          openCollapse ? "block" : "hidden"
-        }`}
-        variants={list}
-        initial="hidden"
-        animate={openCollapse ? "visible" : "hidden"}>
-        <motion.div
-          className="w-full h-auto flex justify-between items-center gap-4 px-4 py-4"
-          variants={item}>
-          <div className="flex-1">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              harum, nesciunt cupiditate placeat enim excepturi ipsa magnam
-              cumque ullam corrupti officia deserunt voluptatum cum illum
-              quibusdam deleniti neque voluptas nostrum tempora est totam nulla?
-              Beatae fugiat molestias saepe, veniam optio modi aut nihil illum,
-              quod ea sed vero, explicabo itaque.
-            </p>
-          </div>
-          <div className="flex-1">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              harum, nesciunt cupiditate placeat enim excepturi ipsa magnam
-              cumque ullam corrupti officia deserunt voluptatum cum illum
-              quibusdam deleniti neque voluptas nostrum tempora est totam nulla?
-              Beatae fugiat molestias saepe, veniam optio modi aut nihil illum,
-              quod ea sed vero, explicabo itaque.
-            </p>
-          </div>
-          <div className="flex-1">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              harum, nesciunt cupiditate placeat enim excepturi ipsa magnam
-              cumque ullam corrupti officia deserunt voluptatum cum illum
-              quibusdam deleniti neque voluptas nostrum tempora est totam nulla?
-              Beatae fugiat molestias saepe, veniam optio modi aut nihil illum,
-              quod ea sed vero, explicabo itaque.
-            </p>
-          </div>
-
+    <AnimatePresence>
+      <div className="w-full h-auto flex flex-col gap-4 overflow-hidden border-y-[0.5px] border-slate-900 pt-8 pb-4">
+        <div className="flex justify-between items-center ">
+          <h1 className="text-2xl font-bold">Mon Portfolio #1</h1>
+          <h2>React, Sass, Framer-motion</h2>
           <h2>2024</h2>
+          <button
+            className="text-white bg-slate-400 px-4 py-2 rounded-md"
+            onClick={handleCollapse}>
+            En voir +
+          </button>
+        </div>
+
+        <motion.div
+          className={`w-full h-auto flex-col overflow-hidden  ${
+            openCollapse ? "visible" : "opacity-0"
+          }`}
+          variants={list}
+          initial="hidden"
+          animate={openCollapse ? "visible" : "hidden"}>
+          <motion.div
+            className="w-full h-auto flex justify-between items-center gap-4 px-4 py-4 bg-indigo-400 rounded-md overflow-hidden"
+            variants={item}>
+            <motion.div
+              className="flex-1 p-2 rounded-md bg-red-400"
+              variants={item}>
+              <Image src={imgprojet} alt="projet" />
+            </motion.div>
+            <motion.div
+              className="flex-1 p-2 rounded-md bg-amber-400"
+              variants={item}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                harum, nesciunt cupiditate placeat enim excepturi ipsa magnam
+                cumque ullam corrupti officia deserunt voluptatum cum illum
+                quibusdam deleniti neque voluptas nostrum tempora est totam
+                nulla? Beatae fugiat molestias saepe, veniam optio modi aut
+                nihil illum, quod ea sed vero, explicabo itaque.
+              </p>
+            </motion.div>
+            <motion.div
+              className="flex-1 p-2 rounded-md bg-lime-400"
+              variants={item}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                harum, nesciunt cupiditate placeat enim excepturi ipsa magnam
+                cumque ullam corrupti officia deserunt voluptatum cum illum
+                quibusdam deleniti neque voluptas nostrum tempora est totam
+                nulla? Beatae fugiat molestias saepe, veniam optio modi aut
+                nihil illum, quod ea sed vero, explicabo itaque.
+              </p>
+            </motion.div>
+          </motion.div>
         </motion.div>
-      </motion.div>
-    </div>
+      </div>
+    </AnimatePresence>
   );
 };
 
